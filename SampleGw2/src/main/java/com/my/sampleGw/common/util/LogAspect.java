f05f40cdb80d3ea45596b34c260566293ec8d714
tenbloodman <tenbloodman@gmail.com>
2018-01-11 14:43:24 +0900

    add transaction

--- SampleGw2/src/main/java/com/my/sampleGw/common/util/LogAspect.java
+++ SampleGw2/src/main/java/com/my/sampleGw/common/util/LogAspect.java
@@ -18,36 +18,46 @@ public class LogAspect {
 
 	private final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
+	private long startTime;
+	private long endTime;
+	
 	@Pointcut("execution(* com.my.sampleGw.*.*.*.*.*Impl.*(..))")
 	private void targetMethod() {};
 
 	@Before("targetMethod()")
 	public void before() {
-		log.info("LogAspect - before");
+		//log.info("LogAspect - before");
+		startTime = System.currentTimeMillis();
 	}
 
 	@AfterReturning(pointcut = "execution(* com.my.sampleGw.*.*.*.*.*Impl.*(..))", returning = "retn")
 	public void afterReturning(JoinPoint joinPoint, Object retn) {
-		log.info("LogAspect - afterReturning - " + retn.toString());
+		//log.info("LogAspect - afterReturning - " + retn.toString());
 	}
 	
 	@After("targetMethod()")
 	public void after(JoinPoint point) {
+		endTime = System.currentTimeMillis();
+		log.info("DurationTime : " + (endTime - startTime));
+		/*
 		log.info("LogAspect - after - " + point.getTarget().getClass().getSimpleName());
 		log.info("LogAspect - after - " + point.getSignature().getName());
 		for (Object item : point.getArgs()) {
 			log.info("LogAspect - after - " + item);
 		}
+		*/
 	}
 
 	@Around("targetMethod()")
 	public Object around(ProceedingJoinPoint point) throws Throwable {
 		Object obj = point.proceed();
+		/*
 		log.info("LogAspect - around - " + point.getTarget().getClass().getSimpleName());
 		log.info("LogAspect - around - " + point.getSignature().getName());
 		for (Object item : point.getArgs()) {
 			log.info("LogAspect - around - " + item);
 		}
+		*/
 		return obj;
 	}
 
